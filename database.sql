8e68145a3c81b700abf9bca0c9bb168c2f4ad365
Kechlimparis <skechav@gmail.com>
2019-01-10 01:43:41 +0200

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,13 @@
+-- Structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
