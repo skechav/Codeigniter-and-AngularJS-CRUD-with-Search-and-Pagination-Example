d1fffa3d9250b07dc5874747cb5b51bbc615366d
Kechlimparis <skechav@gmail.com>
2019-01-10 16:52:25 +0200

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,17 +1,14 @@
 var app =  angular.module('main-App',['ngRoute','angularUtils.directives.dirPagination']);
-app.config( ['$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ) 
-                                                    {
-                                                        $routeProvider.
-                                                            when('/', { 
-                                                                templateUrl: 'templates/home.html', 
-                                                                controller: 'AdminController' } ).
-                                                            when('/items', { 
-                                                                templateUrl: 'templates/items.html', 
-                                                                controller: 'ItemController'           } );
-                                                        
-                                                        // use the HTML5 History API - remove # from url
-                                                        $locationProvider.html5mode({ enabled: true, requireBase: false });
-                                                    }
-            ]
 
-);
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'templates/home.html',
+                controller: 'AdminController'
+            }).
+            when('/items', {
+                templateUrl: 'templates/items.html',
+                controller: 'ItemController'
+            });
+}]);
