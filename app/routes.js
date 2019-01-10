bc5c10029a7f8bd239d893b6f95f73fc6cf8b14c
Kechlimparis <skechav@gmail.com>
2019-01-10 16:20:13 +0200

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,23 +1,17 @@
-var app =  angular.module( 'main-App', ['ngRoute'] );
+var app =  angular.module('main-App',['ngRoute','angularUtils.directives.dirPagination']);
+app.config( ['$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ) 
+                                                    {
+                                                        $routeProvider.
+                                                            when('/', { 
+                                                                templateUrl: 'templates/home.html', 
+                                                                controller: 'AdminController' } ).
+                                                            when('/items', { 
+                                                                templateUrl: 'templates/items.html', 
+                                                                controller: 'ItemController'           } );
+                                                        
+                                                        // use the HTML5 History API - remove # from url
+                                                        $locationProvider.html5mode({ enabled: true, requireBase: false });
+                                                    }
+            ]
 
-app.config(['$routeProvider',
-    function($routeProvider) 
-    {
-        $routeProvider
-            when('/', {
-                templateUrl: 'templates/home.html',
-                controller: 'HomeController'
-            }).
-            when('/users', {
-                templateUrl: 'templates/users.html',
-                controller: 'UserController'
-            }).
-              otherwise({
-                redirectTo: '/'
-            });
-
-	    // use the HTML5 History API
-	    //$locationProvider.html5mode({ enabled: true, requireBase: false });
-	}
-
-]);
+);
